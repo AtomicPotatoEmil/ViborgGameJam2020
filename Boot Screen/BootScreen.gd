71993150478fb3c73da6475f70ae5ce26aebf57b
Emil Valentin <atomicpotato8@gmail.com>
2020-02-08 02:45:26 +0100

    buncha stuff

--- Boot Screen/BootScreen.gd
+++ Boot Screen/BootScreen.gd
@@ -5,3 +5,8 @@ func _ready():
 	$AudioStreamPlayer2D.play()
 	pass
 
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	get_tree().change_scene("res://Menus/Start Menu/StartMenu.tscn")
+	pass
