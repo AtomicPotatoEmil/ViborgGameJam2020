fa5598bc375b0de5e8aabfde7f31b9a1dc32d6bc
Emil Valentin <atomicpotato8@gmail.com>
2020-02-08 18:57:03 +0100

    Probably finished the enemy character

--- Enemy Player/EnemyPlayer.gd
+++ Enemy Player/EnemyPlayer.gd
@@ -3,8 +3,13 @@ extends KinematicBody2D
 var VELOCITY = Vector2()
 var SPEED = 350
 
+var HP = 3
 
-enum {INIT, IDLE, RUN, ATTACK}
+signal enemy_dead
+
+var can_shoot = true
+
+enum {INIT, IDLE, RUN, ATTACK, FIREBALL, HURT, DEAD}
 var state
 
 var anim
@@ -36,6 +41,8 @@ func get_input():
 			
 		if Input.is_action_just_pressed("enemy_melee_attack"):
 			change_state(ATTACK)
+		if Input.is_action_just_pressed("fireball_attack") and can_shoot == true:
+			change_state(FIREBALL)
 		
 	if state == RUN and VELOCITY.x == 0 and VELOCITY.y == 0:
 		change_state(IDLE)
@@ -57,6 +64,12 @@ func change_state(new_state):
 			new_anim = "run"
 		ATTACK:
 			new_anim = "attack"
+		FIREBALL:
+			new_anim = "fireball"
+		HURT:
+			new_anim = "hurt"
+		DEAD:
+			new_anim = "dead"
 	pass
 
 func _physics_process(delta):
@@ -72,11 +85,47 @@ func _physics_process(delta):
 
 
 
-func _on_AnimationPlayer_animation_finished(anim_name):
+func _on_AnimationPlayer_animation_finished(attack):
+	change_state(IDLE)
+	pass
+
+func _on_StartTimer_timeout():
 	change_state(IDLE)
 	pass 
 
 
-func _on_StartTimer_timeout():
+func _on_AnimationPlayerFireball_animation_finished(fireball):
+	if state in [IDLE, FIREBALL]:
+		can_shoot = false
+		$ShootTimer.start()
+		var BALL = preload("res://Enemy Player/Fireball/Fireball.tscn")
+		var b = BALL.instance()
+		b.global_position = $AnimatedSprite/FireballPos.global_position
+		if $AnimatedSprite.scale.x == 1:
+			b.change_direction(-1)
+		else:
+			b.change_direction(1)
+		get_parent().add_child(b)
+		change_state(IDLE)
+		pass 
+
+func hurt():
+	if state in [IDLE, RUN, ATTACK, FIREBALL]:
+		change_state(HURT)
+		HP -= 1
+	pass
+
+
+func _on_ShootTimer_timeout():
+	can_shoot = true
+	pass
+
+
+func _on_AnimationPlayerHurt_animation_finished(hurt):
 	change_state(IDLE)
+	pass 
+
+
+func _on_AnimationPlayerDead_animation_finished(dead):
+	emit_signal("enemy_dead")
 	pass 
---
+++ Enemy Player/Fireball/Fireball.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+var VELOCITY = Vector2()
+var SPEED = 100
+
+var DIRECTION = 1
+
+func _ready():
+	$AnimatedSprite.play()
+
+func _physics_process(delta):
+	VELOCITY.x += SPEED * delta * DIRECTION
+	translate(VELOCITY)
+	pass
+
+func change_direction(dir):
+	DIRECTION = dir
+	pass
+
+
+func _on_Fireball_body_entered(body):
+	if body.is_in_group("player"):
+		body.hurt()
+		queue_free()
+	pass 
