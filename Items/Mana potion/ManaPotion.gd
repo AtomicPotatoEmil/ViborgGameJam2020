43ebc454ebcec0cc1e2e7293c6aeae5675197204
Emil Valentin <atomicpotato8@gmail.com>
2020-02-08 09:39:05 +0100

    added mana potion

---
+++ Items/Mana potion/ManaPotion.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+
+
+func _ready():
+	randomize()
+	$Timer.set_wait_time(range(1,10)[randi()%range(1,10).size()])
+	$Timer.start()
+	pass
+
+
+func _on_Timer_timeout():
+	queue_free()
+	pass 
+
+
+func _on_ManaPotion_area_entered(area):
+	if area.is_in_group("player"):
+		area.gainMana();
+		queue_free()
+	else:
+		queue_free()
+	pass 
