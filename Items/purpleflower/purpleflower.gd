0b2b08b0023926ed1dad5352af8b975a3436e3f8
Emil Valentin <atomicpotato8@gmail.com>
2020-02-09 03:01:44 +0100

    functional splitscreen and world

--- Items/purpleflower/purpleflower.gd
+++ Items/purpleflower/purpleflower.gd
@@ -12,14 +12,6 @@ func _ready():
 	pass # Replace with function body.
 
 
-func _on_purpleflower_body_entered(body):
-	if body.is_in_group("player"):
-		body.gainMana()
-	else:
-		queue_free()
-	pass
-
-
 func _on_pflower_timeout():
 	queue_free()
 
--- Splitscreen/SplitScreen.gd
+++ Splitscreen/SplitScreen.gd
@@ -6,11 +6,13 @@ onready var cam2 = $HBoxContainer/EnemyViewPort/Viewport2/Camera2D
 onready var player_viewport = $HBoxContainer/PlayerViewPort/Viewport
 onready var enemy_viewport = $HBoxContainer/EnemyViewPort/Viewport2
 
-onready var world = $HBoxContainer/PlayerViewPort/Viewport
+onready var world = $HBoxContainer/PlayerViewPort/Viewport/Node2D
 
 func _ready():
 	enemy_viewport.world_2d = player_viewport.world_2d
 	pass
 
 func _physics_process(delta):
+	cam1.global_position = world.get_node("YSort/Player").global_position
+	cam2.global_position = world.get_node("YSort/EnemyPlayer").global_position
 	pass
